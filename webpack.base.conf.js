87c49a876f0001d0add21f9c00b74c40ed0de2ac
zeng2404 <zyy2404@163.com>
2019-11-15 18:13:18 +0800

    pure version

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
     // entry: './src/index.js',
@@ -43,6 +43,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin()
     ]
 };
