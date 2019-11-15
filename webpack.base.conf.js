4236dd854b31417793bd342d35ef83644951da63
zeng2404 <zyy2404@163.com>
2019-11-15 17:41:29 +0800

    first version,create project

---
+++ webpack.base.conf.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    // entry: './src/index.js',
+    entry : {
+        app: ["babel-polyfill", "./src/index.js"]
+    },
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.join(__dirname, '/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+                // use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                use:[{
+                    loader:"babel-loader",
+                    options:{
+                        presets:[
+                            "react","es2015"
+                        ]
+                    }}],
+                exclude:/node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
